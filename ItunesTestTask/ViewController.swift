52505c5b5365cc29e80c7c46452d78510f786b7b
Kypshak09 <amirzhunusov09@gmail.com>
2023-02-28 17:31:34 +0600

    Initial Commit

---
+++ ItunesTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItunesTestTask
+//
+//  Created by Amir Zhunussov on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
